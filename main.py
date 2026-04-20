c254bf5b0c708f94b52c4142ec83708c9f5ee67b
NigraeLunam <101352918+NigraeLunam@users.noreply.github.com>
2026-04-20 20:11:33 -0300

    Soma
    Adicionado um print da soma dos valores coletados anteriormente.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 print("Projeto Calculadora")
 
 a = float(input("Digite o primairo número: "))
-b = float(input("Digite o segundo número: "))
+b = float(input("Digite o segundo número: "))b = float(input("Digite o segundo número: "))
+
+print("Soma: ", a + b)
