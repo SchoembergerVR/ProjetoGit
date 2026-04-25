d81113d041bbef40d4aa16481fcc5019d6e67272
NigraeLunam <101352918+NigraeLunam@users.noreply.github.com>
2026-04-25 13:11:24 -0300

    Inclusão da função main()

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 print("Projeto Calculadora")
 
-a = float(input("Digite o primairo número: "))
-b = float(input("Digite o segundo número: "))
+def main():
+    a = float(input("Digite o primairo número: "))
+    b = float(input("Digite o segundo número: "))
 
-print("Escolha a operação desejada:")
-print("1 - Soma")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Divisão")
+    print("Escolha a operação desejada:")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
 
-operacao = input("Número da operação: ")
+    operacao = input("Número da operação: ")
 
-if operacao == "4" and b == 0:
-    print("Erro: divisão por zero")
-elif operacao == "1":
-    print("Resultado da soma: ", a + b)
-elif operacao == "2":
-    print("Resultado da subtração: ", a - b)
-elif operacao == "3":
-    print("Resultado da multiplicação: ", a * b)
-elif operacao == "4":
-    print("Resultado da divisão: ", a / b)
-else:
-    print("Opção inválida")
+    if operacao == "4" and b == 0:
+        print("Erro: divisão por zero")
+    elif operacao == "1":
+        print("Resultado da soma: ", a + b)
+    elif operacao == "2":
+        print("Resultado da subtração: ", a - b)
+    elif operacao == "3":
+        print("Resultado da multiplicação: ", a * b)
+    elif operacao == "4":
+        print("Resultado da divisão: ", a / b)
+    else:
+        print("Opção inválida")
+
+if __name__ == "__main__":
+    main()
